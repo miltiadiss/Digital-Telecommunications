3dc9d44ffea418c53720a5455706b741e4fd1384
miltiadis <45690339+miltiadiss@users.noreply.github.com>
2024-01-08 15:12:46 +0200

    Update dpcm_decoder.m

--- dpcm_decoder.m
+++ dpcm_decoder.m
@@ -2,20 +2,16 @@
 function [x_reconstructed] = dpcm_decoder(y_hat, p, a_quantized)
     N = length(y_hat);
     x_reconstructed = zeros(N, 1);
+    mem = zeros(p, 1);
+    y_hat_prediction = 0;
 
-    % Calculate the first p values
-    % This assumes that these values are directly transmitted without prediction.
-    for i = 1:p
-        x_reconstructed(i) = y_hat(i);
-    end
-    
     % Calculate the next values from p+1 to N
-    for i = p+1:N
-        % Calculate the prediction y_hat_prediction(n) using the quantized coefficients a_quantized
-        % and the previously reconstructed values
-        y_hat_prediction = a_quantized.' * x_reconstructed(i-1:-1:i-p);
-
+    for i = 1:N
         % Add the prediction to the quantized error to reconstruct x
         x_reconstructed(i) = y_hat_prediction + y_hat(i);
+        % Calculate the prediction y_hat_prediction(n) using the quantized coefficients a_quantized
+        % and the previously reconstructed values
+        mem = [x_reconstructed(i); mem(1:p - 1)];
+        y_hat_prediction = a_quantized.' * mem;
     end
 end
