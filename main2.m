d290ffd054d1e6b11ee8579703f0e906c0c17101
miltiadis <45690339+miltiadiss@users.noreply.github.com>
2024-01-09 03:45:28 +0200

    Update main2.m

--- main2.m
+++ main2.m
@@ -5,32 +5,27 @@ subsetSize = 5000; % Size of each subset
 numSubsets = ceil(numSamples / subsetSize); % Number of subsets
 subsets = cell(numSubsets, 1); % Initialize cell array for subsets
 
+% Define the constants
+min_value = -3.5;
+max_value = 3.5;
+
 for k = 1:numSubsets
     startIdx = (k - 1) * subsetSize + 1;
     endIdx = k * subsetSize;
     subsets{k} = x(startIdx:endIdx);
-    % Questions 2, 4
-    for p = 7:5:12
-        for i = 1:3
-            [a_quantized, y, y_hat, y_hat_prediction] = dpcm_encoder(subsets{k}, p, i, min_value, max_value);
+    % Question 2
+    for p = 5:5:10
+        for N = 1:3
+            [~, ~, y, ~, ~] = dpcm_encoder(subsets{k}, p, N, min_value, max_value);
 
-            figure
-            title(['Subset ', num2str(k), ' - Prediction error and reconstructed signal for p=', num2str(p), ' and N=', num2str(i)]);
-            %legend('x', 'y');
+            figure;
+            plot(subsets{k}, 'b');
+            hold on;
+            plot(y, 'r');
+            hold off;
         
-            subplot(3,1,1); % Create a subplot for signal y
-            plot(y);
-            xlabel('Prediction Error');
-
-            subplot(3,1,2); % Create a subplot for signal x
-            plot(subsets{k});
-            xlabel('Initial Signal');
-
-            x_reconstructed = dpcm_decoder(y_hat, p, a_quantized);
-
-            subplot(3,1,3); % Create a subplot for signal x_recondtructed
-            plot(x_reconstructed);
-            xlabel('Reconstructed Signal');
+            title(['Subset ', num2str(k), ' - Prediction error and initial signal for p=', num2str(p), ' and N=', num2str(N)]);
+            legend('Initial Signal', 'Prediction Error');
         end
     end
 
@@ -40,20 +35,23 @@ for k = 1:numSubsets
 
     % Loop over each p and N value
     for p = 5:10
+        result_string = strcat(' p = ', num2str(p));
+        disp(result_string);
         for N = 1:3
-            [~, ~, ~, y_hat_prediction] = dpcm_encoder(subsets{k}, p, N, min_value, max_value);
-            currentMSE = mean((subsets{k} - y_hat_prediction).^2);  % Calculate current MSE
+            [~, a, y, ~, ~] = dpcm_encoder(subsets{k}, p, N, min_value, max_value);
+            disp(a);
+            currentMSE = mean(y.^2);  % Calculate current MSE
             mse_matrix(p - 4, N) = currentMSE;  % Assign to the matrix
         end
     end
 
-    % Define a set of 6 distinct colors
+    % Define a set of colors
     colors = [1, 0, 0; % Red
-              0, 1, 0; % Green
-              0, 0, 1; % Blue
-              1, 1, 0; % Yellow
-              1, 0, 1; % Magenta
-              0, 1, 1]; % Cyan
+            0, 1, 0; % Green
+            0, 0, 1; % Blue
+            1, 1, 0; % Yellow
+            1, 0, 1; % Magenta
+            0, 1, 1]; % Cyan
 
     figure;
     hold on;
@@ -70,8 +68,22 @@ for k = 1:numSubsets
     legend('p = 5', 'p = 6', 'p = 7', 'p = 8', 'p = 9', 'p = 10');
 
     hold off;
+
+    % Question 4
+    for p = 5:5:10
+        for N = 1:3
+            [a_quantized, ~, ~, y_hat, ~] = dpcm_encoder(subsets{k}, p, N, min_value, max_value);
+            x_reconstructed = dpcm_decoder(y_hat, p, a_quantized);
+
+            figure;
+            subplot(2,1,1); 
+            plot(subsets{k});
+            title(['Reconstructed signal and initial signal for p=', num2str(p), ' and N=', num2str(N)]);
+            xlabel('Initial Signal');
+
+            subplot(2,1,2); 
+            plot(x_reconstructed);
+            xlabel('Reconstructed Signal');
+        end
+    end
 end
-
-
-
-
